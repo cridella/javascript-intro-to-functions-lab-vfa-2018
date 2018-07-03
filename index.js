b32a8972a139bc67b769e69fe5daad26564b0ff5
Christina Ridella <cridella3@gmail.com>
2018-07-03 12:57:33 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,11 @@
+function shout(strint) {
+  return string.toUpperCase ()
+} 
+function whisper(string) {
+  return string.toLowerCase()
+}
+function logShout(string) {
+  var spy
+  spy= 'Hello'
+  console.log(spy); string.toUpperCase;
+}
